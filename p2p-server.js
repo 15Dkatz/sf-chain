08b17cc0fab3c20c86ba5ec43902846ee15f5803
David Katz <15Dkatz@shcp.edu>
2018-01-26 10:33:32 -0800

    Add notes for p2p connection

--- p2p-server.js
+++ p2p-server.js
@@ -37,7 +37,7 @@ class P2PServer {
   errorHandler(socket) {
     const closeConnection = (socket) => {
       console.log(`Connection failed to peer: ${socket.url}`);
-      this.sockets.splice(sockets.indexOf(socket), 1);
+      this.sockets.splice(this.sockets.indexOf(socket), 1);
     };
 
     socket.on('close', () => closeConnection(socket));
@@ -53,49 +53,14 @@ class P2PServer {
     });
   }
 
-  // TODO: create a function that syncs up all the chains
+  /*
+    TODO: create a function that syncs up all the chains on start up
+    Why would this be necessary?
+    Could you sync on add block?
+    Oh, what if you add a peer after the fact. For example, the chain is already 3 blocks long
+    then a new peer is connected and started
+    Then run a get of the blocks on all (3 or so) connections to check that the blocks are synced
+  */
 }
 
 module.exports = P2PServer;
-// const sockets = [];
-
-// const initP2PServer = () => {
-//   const server = new Websocket.Server({ port: P2P_PORT });
-//   server.on('connection', socket => connectSocket(socket));
-//   console.log(`Listening for peer to peer connections on: ${P2P_PORT}`);
-// }
-
-// const connectSocket = (socket) => {
-//   sockets.push(socket);
-//   messageHandler(socket);
-//   // TODO: necessary to include?
-//   errorHandler(socket);
-// }
-
-// const messageHandler = (socket) => {
-//   socket.on('message', data => {
-//     const message = JSON.parse(data);
-
-//     console.log(`Received socket message: ${message}.`);
-//   });
-// }
-
-// // TODO: necessary to include?
-// const errorHandler = (socket) => {
-//   const closeConnection = (socket) => {
-//     console.log(`Connection failed to peer: ${socket.url}`);
-//     sockets.splice(sockets.indexOf(socket), 1);
-//   };
-
-//   socket.on('close', () => closeConnection(socket));
-//   socket.on('error', () => closeConnection(socket));
-// }
-
-// const connectToPeers = (peers) => {
-//   peers.forEach(peer => {
-//     const socket = new Websocket(peer);
-
-//     socket.on('open', () => initConnection(socket));
-//     ws.on('error', () => console.log('connection failed'));
-//   });
-// }
