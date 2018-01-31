3117f3a5eeece7b4b0698bb0c0a2e49fe6397287
David Katz <15Dkatz@shcp.edu>
2018-01-31 09:43:04 -0800

    Test the wallet

--- wallet.test.js
+++ wallet.test.js
@@ -1,21 +1,77 @@
 const Wallet = require('./wallet');
 const Blockchain = require('./blockchain');
 const Transaction = require('./transaction');
+const TransactionPool = require('./transaction-pool');
 
 describe('Wallet', () => {
   let wallet;
+  let bc;
+  let tp;
 
   beforeEach(() => {
     wallet = new Wallet();
+    bc = new Blockchain();
+    tp = new TransactionPool();
+
   });
 
-  it('calculates the correct balance', () => {
+  describe('creating a transaction', () => {
+    let transaction;
+    let sendAmount;
+    let recipient;
+
+    beforeEach(() => {
+      sendAmount = 50;
+      recipient = 'r4nd0m-4ddr3s';
+      transaction = wallet.createTransaction(recipient, sendAmount, bc, tp);
+    });
+
+    it('inputs the balance of the wallet', () => {
+      expect(transaction.input.address).toEqual(wallet.publicKey);
+    });
+
+    it('ouputs the `sendAmount` subtracted from the wallet balance', () => {
+      expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount)
+        .toEqual(wallet.balance - sendAmount);
+    });
+
+
+    it('outputs the `sendAmount` added to the recipient', () => {
+      expect(transaction.outputs.find(output => output.address === recipient).amount)
+        .toEqual(sendAmount);
+    });
+
+    describe('and doing the same transaction', () => {
+      beforeEach(() => {
+        const transaction2 = wallet.createTransaction(recipient, sendAmount, bc, tp);
+      });
+
+      it('doubles the amount subtracted from the wallet balance', () => {
+        expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount)
+          .toEqual(wallet.balance - sendAmount*2);
+      });
+
+
+      it('copies the `sendAmount` output for the recipient', () => {
+        expect(
+          transaction.outputs.filter(output => output.address === recipient)
+          .map(output => output.amount)
+        ).toEqual([sendAmount, sendAmount]);
+      });
+    });
+  });
+
+  it('calculates the balance for blockchain transactions matching this wallet', () => {
+    const senderWallet = new Wallet();
     const addBalance = 100;
-    // const senderWallet = new Wallet();
-    // const bc = new Blockchain();
+    const repeatAdd = 3;
+    const blockData = [];
 
-    // bc.addBlock([Transaction.normalTransaction(senderWallet, wallet.publicKey, addBalance)]);
+    for (let i=0; i<repeatAdd; i++) {
+      blockData.push(Transaction.normalTransaction(senderWallet, wallet.publicKey, addBalance));
+    }
 
-    // console.log('wallet balance', wallet.calculateBalance(bc));
+    bc.addBlock(blockData);
+    expect(wallet.calculateBalance(bc)).toEqual(addBalance * repeatAdd);
   });
 });
