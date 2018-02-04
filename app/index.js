0b67375ff00b2a2e0404f069a596213e5c421325
David Katz <15Dkatz@shcp.edu>
2018-02-03 16:30:32 -0800

    Remove unnecessary line

--- app/index.js
+++ app/index.js
@@ -4,7 +4,6 @@
    as the blockchain itself udpates.
 */
 
-// Why are these all on different ports?
 /*
 HTTP_PORT=3001 P2P_PORT=5001 npm run dev
 HTTP_PORT=3002 P2P_PORT=5002 PEERS=ws://localhost:5001 npm run dev
